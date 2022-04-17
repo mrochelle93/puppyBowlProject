3c8a3ec367f5db6fb2159f211e7bc3c57989180a
mrochelle93 <99699122+mrochelle93@users.noreply.github.com>
2022-04-17 17:40:31 -0400

    Add files via upload

---
+++ UNIV_PuppyBowl_Starter-main/client/ajaxHelpers.js
@@ -0,0 +1,14 @@
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = "FULLSTACK";
+// Use the APIURL variable for fetch requests
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+
+export const fetchAllPlayers = async () => {};
+
+export const fetchSinglePlayer = async (playerId) => {};
+
+export const addNewPlayer = async (playerObj) => {};
+
+export const removePlayer = async (playerId) => {};
+
+.json()
